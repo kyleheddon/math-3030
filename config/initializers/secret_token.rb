2c9d448dac06801d015bb5d9182c73b1d3c0be86
kyleheddon <kyle.heddon@careerbuilder.com>
2013-04-09 14:28:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirtyThirty::Application.config.secret_token = 'd81d61aebfad918e89063696d86eb766740fc0bc183560fad2280ac3f2447877fd2640615331923e3aa95d15b43ee4553d456a0c181e35351257fda8e0470f9d'
